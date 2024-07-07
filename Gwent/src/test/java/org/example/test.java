96ae07f01c823590d2e6d63586b2668b6aa686d7
FoadKhab <f.kh.20051384@gmail.com>
2024-07-07 15:10:26 +0430

    UpdateFriendRequestMessage.java

--- Gwent/src/test/java/org/example/test.java
+++ Gwent/src/test/java/org/example/test.java
@@ -1,12 +1,38 @@
 package org.example;
 
+import controller.loginController.LoginMenuController;
+import javafx.application.Platform;
 import model.User;
 import model.enums.loginMenu.ConfirmQuestions;
 import org.junit.Before;
+import org.junit.Test;
 
-class test{
+public class test {
+    private boolean temp = true;
+
     @Before
     public void setUp() {
-        User user = new User(ConfirmQuestions.q1, "testUsername", "testPass", "nickname", "email" , "answer");
+
+        if (temp) {
+            Platform.startup(() -> {
+                User user = new User(ConfirmQuestions.q1, "testUsername", "testPass", "nickname", "email", "answer");
+                temp = false;
+            });
+        } else {
+            User user = new User(ConfirmQuestions.q1, "testUsername", "testPass", "nickname", "email", "answer");
+        }
+    }
+
+    //test login menu
+
+    //test method checkAllErrors
+    @Test
+    public void isUsernameAllreadyUsedTest() {
+        assert !LoginMenuController.checkAllErrors("testUsername", "testPass", "testPass", "nickname", "email").isSuccessful();
+    }
+
+    @Test
+    public void validUsernameTest() {
+        assert !LoginMenuController.checkAllErrors("testUsername", "testPass", "testPass", "nickname", "email").isSuccessful();
     }
 }
--- GwentNetwork/src/main/java/message/server/UpdateFriendRequestMessage.java
+++ GwentNetwork/src/main/java/message/server/UpdateFriendRequestMessage.java
@@ -1,8 +1,33 @@
 package message.server;
 
+import server.model.toolClasses.Result;
+
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
 public class UpdateFriendRequestMessage extends ServerMessage{
+    private ArrayList<String> friends;
+    private ArrayList<String> friendRequests;
 
-    public UpdateFriendRequestMessage() {
+    public UpdateFriendRequestMessage(ArrayList<String> friends, ArrayList<String> friendRequests, Result result) {
+        super(result);
+        this.friends = friends;
+        this.friendRequests = friendRequests;
+    }
 
+    public ArrayList<String> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(ArrayList<String> friends) {
+        this.friends = friends;
+    }
+
+    public ArrayList<String> getFriendRequests() {
+        return friendRequests;
+    }
+
+    public void setFriendRequests(ArrayList<String> friendRequests) {
+        this.friendRequests = friendRequests;
     }
 }
