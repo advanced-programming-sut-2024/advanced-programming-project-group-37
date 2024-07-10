63d58e5bcb9c099f57e4dad185281762c061887d
FoadKhab <f.kh.20051384@gmail.com>
2024-07-10 23:54:02 +0430

    email message

---
+++ GwentNetwork/src/main/java/message/client/LoginMenu/EmailVerify.java
@@ -0,0 +1,18 @@
+package message.client.LoginMenu;
+
+import message.client.ClientMessage;
+import message.client.MessageType;
+
+public class EmailVerify extends ClientMessage {
+    String code;
+    String email;
+    public EmailVerify(String code, String email) {
+        this.email = email;
+        this.code = code;
+        this.type = MessageType.VERIFY_EMAIL;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
