abc77e8b076ad70512f570284e7f0ad8a33a84eb
FoadKhab <f.kh.20051384@gmail.com>
2024-07-11 12:56:57 +0430

    تموم شو لعنتی 6

--- GwentNetwork/src/main/java/message/client/Game/LastState.java
+++ GwentNetwork/src/main/java/message/client/Game/LastState.java
@@ -2,12 +2,14 @@ package message.client.Game;
 
 import javafx.scene.image.Image;
 import message.client.ClientMessage;
+import message.client.MessageType;
 
 public class LastState extends ClientMessage {
     private Image image;
 
     public LastState(Image image, String token) {
         this.image = image;
+        this.type = MessageType.LAST_STATE;
         this.token = token;
     }
 
